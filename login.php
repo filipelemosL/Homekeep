40453b506d341245a84110df0069bec0cb4c5b42
filipelemosL <filipelemos1415@gmail.com>
2023-03-20 13:09:18 -0300

    elementos principais da tela de login adicionados

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles\login.css">
+    <title>Homekeep</title>
+</head>
+<body>
+    <section>
+        <forms class='forms'>
+            <input class='formStyle' type='text' name='email'>
+            <input class='formStyle' type='password' name='password'>
+            <input class='subButton' type='submit'>
+        </forms>
+    </section>
+</body>
+</html>
