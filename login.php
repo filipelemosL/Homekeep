c08ac931cb434aa74b2dcf6e07b2a64d39dff726
filipelemosL <filipelemos1415@gmail.com>
2023-03-20 13:51:01 -0300

    tela de login estilizada com título, formulários e botão prontos

--- login.php
+++ login.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles\login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@800&family=League+Spartan:wght@600&display=swap" rel="stylesheet">
     <title>Homekeep</title>
 </head>
 <body>
     <section>
+        <h1 class='title'>Homekeep<h1>
         <forms class='forms'>
             <input class='formStyle' type='text' name='email'>
             <input class='formStyle' type='password' name='password'>
