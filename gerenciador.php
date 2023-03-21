718de4fb2ea4d611cb6adaf51a1d2976f63278c4
filipelemosL <filipelemos1415@gmail.com>
2023-03-21 17:41:06 -0300

    adicionada página de gerenciamento

---
+++ gerenciador.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles\gerenciador.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@800&family=League+Spartan:wght@600&display=swap" rel="stylesheet">
+    <title>Homekeep</title>
+</head>
+<body>
+    <header>
+        <h1 class='title'>Homekeep</h1>
+        <div class='despesa'>
+            <h2 class='result_despesa'>
+        </div>
+        <div class='disponivel'>
+            <h2 class='result_disponivel'>
+        </div>
+    </header>
+    <section>
+    </section>
